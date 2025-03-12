2006974f8e273e54bd08087460212e9a662f35a9
Shriharsha Mogra <harshamogra@gmail.com>
2025-03-12 09:31:21 +0530

    Task 4 done...

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello, PES2UG22CS542\n";
+  cout << "Hello, Shriharsha\n ";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
